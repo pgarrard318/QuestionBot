fa86f6a68e9914d73825254054f7ae8c8633f6a9
pgarrard318 <pgarrard@ves.org>
2021-09-16 08:48:31 -0400

    Initial Commit

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,20 @@
+/*
+
+ */
+
+
 struct MyQuestionAnswerer {
+  
     func responseTo(question: String) -> String {
-        // TODO: Write a response
-        return "?"
+        if question.hasPrefix("What") {
+            return "I am God, an all seeing create."
+        } else if question.hasPrefix("hello") {
+            return "Hello mortal"
+        } else if question.hasPrefix("tell") {
+            return "I'm not sure, ask another question"
+        } else {
+            return "I'm sorry, I dont understand the question"
+        }
+        
     }
 }
